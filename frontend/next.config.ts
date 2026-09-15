7def0082eb7916add3a334b22b7867d2747de665
John-Henry Ross <john-henry@warpdevelopment.com>
2026-09-15 20:16:46 +0200

    fix(frontend): allow public domain for development resources

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: process.env.SITE_DOMAIN ? [process.env.SITE_DOMAIN] : [],
   images: {
     remotePatterns: [
       {
